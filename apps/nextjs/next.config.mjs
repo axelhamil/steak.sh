2cfa06f688ee94b8db613c130688c3f04e28f0e4
Axel Hamilcaro <axel.hamilcaro@icloud.com>
2025-06-21 21:57:38 +0200

    feat: remove dockerfile

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -2,7 +2,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   transpilePackages: ["@packages/libs", "@packages/ui", "@packages/drizzle"],
 };
 
